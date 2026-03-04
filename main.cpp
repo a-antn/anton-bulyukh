eb4aed4c8011ff2e326f39b976a3a4d39970a9c8
Булыух Антон ТКИ-141 <antonbulyuh@yandex.ru>
2026-03-04 14:08:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include "circle.h"
+#include "point.h"
 
+/**
+ * @brief Считывает введенное значение
+ * @params message Сообщение пользователю
+ * @returns Введенное значение
+ */
 double getValue(std::string message);
+
 /**
  * @brief Точка входа в программу
  * @return 0 в случае успеха
  */
 int main () {
-    double center_x = getValue("Enter centerX");
-    double center_y = getValue("Enter centerY");
+    double center_x = getValue("Enter centerX: ");
+    double center_y = getValue("Enter centerY: ");
     Point center(center_x, center_y);
     double radius = getValue("Enter radius");
     Circle myCircle(center, radius);
@@ -15,4 +22,11 @@ int main () {
     std::cout<<"Length is: "<<myCircle.GetLength()<<std::endl;
     
     return 0;
+}
+
+double getValue(std::string message) {
+    double value;
+    std::cout<<message;
+    std::cin>>value;
+    return value;
 }
